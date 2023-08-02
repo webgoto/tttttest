3872034e232cbcf778cae6fa6592b8ab92710f6c
webgoto <4490262+webgoto@users.noreply.github.com>
2023-08-02 19:11:32 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# 現在の日時を取得
+datetime=$(date +"%Y%m%d%H%M%S")
+
+# ファイル名を作成
+filename="file_$datetime.txt"
+
+# テキスト内容を定義
+text="This is the content of the file."
+
+# ファイルを作成し、テキストを書き込む
+echo "$text" > "$filename"
+
+echo "ファイル $filename を作成しました。"
